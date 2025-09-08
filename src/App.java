483424fd66ef5bedf96e610725a6c378ca155cc9
AniketRatan27 <ratan.aniket09@gmail.com>
2025-09-08 20:44:59 +0530

    problem one of 75th day

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import CodeChallenges.ArrayStrictlyIncreasing;
 import CodeChallenges.ConcatenateString;
 import CodeChallenges.DistinctCarFeature;
 import CodeChallenges.LongestConsecutiveCharacters;
@@ -9,6 +10,8 @@ import CodeChallenges.TestScores;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        ArrayStrictlyIncreasing increasing = new ArrayStrictlyIncreasing();
+        int[] nums = { 2, 3, 1, 2 };
+        System.out.println(increasing.canBeIncreasing(nums));
     }
 }
